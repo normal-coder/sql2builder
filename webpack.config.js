ce8ecc1526c9e92c5e8f512434b117f222a6675e
诺墨 <normal@normalcoder.com>
2022-12-21 22:45:33 +0800

    build(webpack): adjust the build directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./src/bootstrap.js",
-  output: {
-    path: path.resolve(__dirname, ""),
-    filename: "bootstrap.js",
-  },
-  mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['./src/index.html'])
-  ],
+    entry: "./src/bootstrap.js",
+    output: {
+        path: path.resolve(__dirname, "./build"),
+        filename: "bootstrap.js",
+    },
+    mode: "development",
+    plugins: [
+        new CopyWebpackPlugin(['./src/index.html'])
+    ],
 };
